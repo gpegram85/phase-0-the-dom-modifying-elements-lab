1d08eb02c60b320c46f55977efde6a16f00828fd
Grey Pegram <dpegram85@gmail.com>
2023-12-19 20:11:56 -0500

    Completed Labs

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const rmMain = document.getElementById('main');
+rmMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+
+const textNode = document.createTextNode("Grey is the champion");
+newHeader.appendChild(textNode);
